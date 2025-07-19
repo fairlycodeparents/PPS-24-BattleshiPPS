14d31dc65b845337d297626997af4e65fff22b94
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2025-07-19 11:10:01 +0200

    feat: simple GUI for ship positioning (move)

---
+++ src/main/scala/it/unibo/shipps/controller/GameController.scala
@@ -0,0 +1,37 @@
+package it.unibo.shipps.controller
+
+import it.unibo.shipps.model.*
+import it.unibo.shipps.view.SimpleGui
+import scala.swing.Swing
+
+case class GameState(board: PlayerBoard, selectedShip: Option[Ship])
+
+class GameController(
+    initialBoard: PlayerBoard,
+    val positioning: ShipPositioning,
+    var view: SimpleGui
+):
+
+  private var state = GameState(initialBoard, None)
+  private def handleCellClick(state: GameState, pos: Position): GameState =
+    state.selectedShip match
+      case None =>
+        positioning.getShipAt(state.board, pos) match
+          case Right(ship) => state.copy(selectedShip = Some(ship))
+          case Left(_)     => state
+      case Some(ship) =>
+        positioning.moveShip(state.board, ship, pos) match
+          case Right(updatedBoard) => GameState(updatedBoard, None)
+          case Left(_)             => state
+
+  def onCellClick(pos: Position): Unit =
+    val newState = handleCellClick(state, pos)
+    state = newState
+    Swing.onEDT {
+      view.update(state.board, state.selectedShip)
+    }
+
+  def onCellDoubleClick(pos: Position): Unit =
+    state.board.getShips.find(_.getPositions.contains(pos)).foreach { ship =>
+      // TODO: rotate
+    }
