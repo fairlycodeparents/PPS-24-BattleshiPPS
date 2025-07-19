577c8e4192665cec302dbea8f7fb44c4e003e49f
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2025-07-19 11:39:07 +0200

    refactor: simplify GameController by merging click logic

--- src/main/scala/it/unibo/shipps/controller/GameController.scala
+++ src/main/scala/it/unibo/shipps/controller/GameController.scala
@@ -7,44 +7,32 @@ import scala.swing.Swing
 case class GameState(board: PlayerBoard, selectedShip: Option[Ship])
 
 class GameController(
-    initialBoard: PlayerBoard,
-    val positioning: ShipPositioning,
-    var view: SimpleGui
-):
+                      initialBoard: PlayerBoard,
+                      val positioning: ShipPositioning,
+                      var view: SimpleGui
+                    ):
 
   private var state = GameState(initialBoard, None)
-  private def handleCellClick(state: GameState, pos: Position): GameState =
-    state.selectedShip match
+
+  private def handleCellAction(pos: Position)(
+    shipAction: (PlayerBoard, Ship, Position) => Either[String, PlayerBoard]
+  ): Unit =
+    state = state.selectedShip match
       case None =>
         positioning.getShipAt(state.board, pos) match
           case Right(ship) => state.copy(selectedShip = Some(ship))
           case Left(_)     => state
       case Some(ship) =>
-        positioning.moveShip(state.board, ship, pos) match
+        shipAction(state.board, ship, pos) match
           case Right(updatedBoard) => GameState(updatedBoard, None)
           case Left(_)             => state
+    updateView()
 
-  private def handleDoubleCellClick(state: GameState, pos: Position): GameState =
-    state.selectedShip match
-      case None =>
-        positioning.getShipAt(state.board, pos) match
-          case Right(ship) => state.copy(selectedShip = Some(ship))
-          case Left(_) => state
-      case Some(ship) =>
-        positioning.rotateShip(state.board, ship) match
-          case Right(updatedBoard) => GameState(updatedBoard, None)
-          case Left(_) => state
+  private def updateView(): Unit =
+    Swing.onEDT(view.update(state.board, state.selectedShip))
 
   def onCellClick(pos: Position): Unit =
-    val newState = handleCellClick(state, pos)
-    state = newState
-    Swing.onEDT {
-      view.update(state.board, state.selectedShip)
-    }
+    handleCellAction(pos)(positioning.moveShip)
 
   def onCellDoubleClick(pos: Position): Unit =
-    val newState = handleDoubleCellClick(state, pos)
-    state = newState
-    Swing.onEDT {
-      view.update(state.board, state.selectedShip)
-    }
+    handleCellAction(pos) { (board, ship, _) => positioning.rotateShip(board, ship) }
