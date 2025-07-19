f8040ae4627842f758362c9599c743971f8d73ac
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2025-07-19 10:59:55 +0200

    feat(ship): add rotateShip method

--- src/main/scala/it/unibo/shipps/model/ShipPositioning.scala
+++ src/main/scala/it/unibo/shipps/model/ShipPositioning.scala
@@ -27,6 +27,13 @@ trait ShipPositioning:
     */
   def moveShip(board: PlayerBoard, ship: Ship, position: Position): Either[String, PlayerBoard]
 
+  /** Rotates a ship on the player board.
+    * @param board the [[PlayerBoard]] to rotate the ship on
+    * @param ship  the [[Ship]] to be rotated
+    * @return an [[Either]] containing an error message if the rotation fails, or the updated [[PlayerBoard]]
+    */
+  def rotateShip(board: PlayerBoard, ship: Ship): Either[String, PlayerBoard]
+
   /** Places a ship on the player board at the specified position.
     * @param board the [[PlayerBoard]] to place the ship on
     * @param ship  the [[Ship]] to be placed
@@ -51,25 +58,52 @@ trait ShipPositioning:
 /** Companion object for [[ShipPositioning]]. */
 object ShipPositioningImpl extends ShipPositioning:
 
+  /** Checks if the ship is out of bounds of the player board.
+   *
+   * @param ship the [[Ship]] to check
+   * @return an [[Either]] containing an error message if the ship is out of bounds, or unit if it is within bounds
+   */
+  private def checkBounds(ship: Ship): Either[String, Unit] =
+    if !isShipOutOfBounds(ship) then Right(())
+    else Left("Ship is out of bounds.")
+
+  /** Checks if the ship overlaps with any existing ships on the board.
+   *
+   * @param board the [[PlayerBoard]] to check against
+   * @param ship  the [[Ship]] to check for overlap
+   * @return an [[Either]] containing an error message if there is an overlap, or unit if no overlap exists
+   */
+  private def checkOverlap(board: PlayerBoard, ship: Ship): Either[String, Unit] =
+    if !board.isAnyPositionOccupied(ship.getPositions) then Right(())
+    else Left("Ship overlaps with another ship.")
+
+  /** Generic method to shift a ship on the board.
+   * @param board           the [[PlayerBoard]] containing the ship
+   * @param ship            the original [[Ship]] to transform
+   * @param shift           function that shift the ship
+   * @return an [[Either]]  containing error message if shift fails, or updated [[PlayerBoard]]
+   */
+  private def ShiftShip(
+                             board: PlayerBoard,
+                             ship: Ship,
+                             shift: Ship => Ship
+                           ): Either[String, PlayerBoard] =
+    for
+      boardWithoutShip <- Right(board.removeShip(ship))
+      shiftedShip <- Right(shift(ship))
+      updatedBoard <- placeShip(boardWithoutShip, shiftedShip)
+    yield updatedBoard
+
   override def isShipOutOfBounds(ship: Ship): Boolean =
     ship.getPositions.exists(pos =>
       pos.x < 0 || pos.x >= PlayerBoard.size || pos.y < 0 || pos.y >= PlayerBoard.size
     )
 
   override def validateShipPlacement(board: PlayerBoard, ship: Ship): Either[String, Unit] =
-    if isShipOutOfBounds(ship) then
-      Left("Ship is out of bounds.")
-    else if board.isAnyPositionOccupied(ship.getPositions) then
-      Left("Ship overlaps with another ship.")
-    else
-      Right(())
-
-  override def moveShip(board: PlayerBoard, ship: Ship, position: Position): Either[String, PlayerBoard] =
-    for {
-      boardWithoutShip <- Right(board.removeShip(ship))
-      movedShip        <- Right(ship.move(position))
-      updatedBoard     <- placeShip(boardWithoutShip, movedShip)
-    } yield updatedBoard
+    for
+      _ <- checkBounds(ship)
+      _ <- checkOverlap(board, ship)
+    yield ()
 
   override def placeShip(board: PlayerBoard, ship: Ship): Either[String, PlayerBoard] =
     validateShipPlacement(board, ship).map(_ => board.addShip(ship))
@@ -78,6 +112,12 @@ object ShipPositioningImpl extends ShipPositioning:
     board.getShips
       .find(_.getPositions.contains(selectedShip))
       .toRight("No ship found at the selected position.")
+
+  override def moveShip(board: PlayerBoard, ship: Ship, position: Position): Either[String, PlayerBoard] =
+    ShiftShip(board, ship, _.move(position))
+
+  override def rotateShip(board: PlayerBoard, ship: Ship): Either[String, PlayerBoard] =
+    ShiftShip(board, ship, _.rotate)
 
   override def randomPositioning(board: PlayerBoard, ships: List[Ship]): Either[String, PlayerBoard] =
     val maxAttempts = 1000
