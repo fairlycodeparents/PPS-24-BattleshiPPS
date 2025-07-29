495435b251c24c69b0741ef6f413e6952ff988fd
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-07-29 11:46:42 +0200

    refactor(view): improve GameSetupFrame code style

--- src/main/scala/it/unibo/shipps/view/GameSetupFrame.scala
+++ src/main/scala/it/unibo/shipps/view/GameSetupFrame.scala
@@ -1,26 +1,32 @@
 package it.unibo.shipps.view
 
 import it.unibo.shipps.model.ShipType
-
 import scala.swing.*
 import scala.swing.event.*
 import javax.swing.{JSpinner, SpinnerNumberModel}
 
 object HomeFrame:
+
+  private val maxShipCount    = 5
+  private val minShipCount    = 0
+  private val stepShipCount   = 1
+  private val framePadding    = 30
+  private val verticalSpacing = 10
+
   def top: MainFrame = new MainFrame:
     title = "BattleshiPPS: Homepage"
 
     val singlePlayerButton = new Button("Play vs Computer")
     val multiPlayerButton  = new Button("Multiplayer")
+
     val spinners: Map[ShipType, JSpinner] = ShipType.values.map(ship =>
-      ship -> new JSpinner(new SpinnerNumberModel(0, 0, 5, 1))
+      ship -> new JSpinner(new SpinnerNumberModel(minShipCount, minShipCount, maxShipCount, stepShipCount))
     ).toMap
 
     val shipsSettingsPanel: GridPanel = new GridPanel(ShipType.values.length, 2):
-      for (shipType <- ShipType.values) {
+      for shipType <- ShipType.values do
         contents += new Label(shipType.toString)
         contents += Component.wrap(spinners(shipType))
-      }
 
     val buttonPanel: FlowPanel = new FlowPanel:
       contents += singlePlayerButton
@@ -28,9 +34,9 @@ object HomeFrame:
 
     contents = new BoxPanel(Orientation.Vertical):
       contents += shipsSettingsPanel
-      contents += Swing.VStrut(10)
+      contents += Swing.VStrut(verticalSpacing)
       contents += buttonPanel
-      border = Swing.EmptyBorder(30, 30, 10, 30)
+      border = Swing.EmptyBorder(framePadding)
 
     listenTo(singlePlayerButton, multiPlayerButton)
     reactions += {
