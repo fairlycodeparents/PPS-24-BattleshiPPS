76b46edea338cd2f58c7aeb3d31d69f0526be0b3
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-08-04 10:48:04 +0200

    feat: implement start game button handlers

--- src/main/scala/it/unibo/shipps/view/GameSetupFrame.scala
+++ src/main/scala/it/unibo/shipps/view/GameSetupFrame.scala
@@ -1,12 +1,13 @@
 package it.unibo.shipps.view
 
-import it.unibo.shipps.model.{DefaultConfiguration, ShipType}
+import it.unibo.shipps.controller.{GameController, Turn}
+import it.unibo.shipps.model.*
 
+import javax.swing.{JSpinner, SpinnerNumberModel}
 import scala.swing.*
 import scala.swing.event.*
-import javax.swing.{JSpinner, SpinnerNumberModel}
 
-object HomeFrame:
+object GameSetupFrame:
 
   private val maxShipCount    = 5
   private val minShipCount    = 0
@@ -49,19 +50,41 @@ object HomeFrame:
       contents += singlePlayerButton
       contents += multiPlayerButton
 
-    contents = new BoxPanel(Orientation.Vertical):
+    contents = new BoxPanel(scala.swing.Orientation.Vertical):
       contents += shipsSettingsPanel
       contents += Swing.VStrut(verticalSpacing)
       contents += buttonPanel
       border = Swing.EmptyBorder(framePadding)
 
+    private def createBoard(): PlayerBoard =
+      val ships = ShipType.values.toSeq.flatMap(shipType =>
+        val num = spinners(shipType).getValue.asInstanceOf[Int]
+        Seq.fill(num)(shipType.at(Position(0, 0), it.unibo.shipps.model.Orientation.Horizontal))
+      )
+        .toList
+      ShipPositioningImpl
+        .randomPositioning(PlayerBoard(), ships)
+        .getOrElse(throw new IllegalStateException("Invalid ship positioning"))
+
+    private def createController(secondPlayer: Player): Unit =
+      val controller = GameController(
+        createBoard(),
+        createBoard(),
+        HumanPlayer(),
+        secondPlayer,
+        ShipPositioningImpl,
+        null
+      )
+      val view = new SimpleGui(controller)
+      controller.view = view
+      view.update(Turn.FirstPlayer)
+      view.visible = true
+      this.close()
+
     listenTo(singlePlayerButton, multiPlayerButton)
     reactions += {
-      case ButtonClicked(`singlePlayerButton`) => println("Start a SinglePlayer game")
-      case ButtonClicked(`multiPlayerButton`)  => println("Start a MultiPlayer game")
+      case ButtonClicked(`singlePlayerButton`) =>
+        createController(BotPlayer(RandomBotAttackStrategy()))
+      case ButtonClicked(`multiPlayerButton`) =>
+        createController(HumanPlayer())
     }
-
-@main def run(): Unit =
-  Swing.onEDT {
-    HomeFrame.top.visible = true
-  }
