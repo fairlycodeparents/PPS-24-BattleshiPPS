faec8a6f2218952e0c40bcd8dec7cf9fc13b5654
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-08-13 11:24:06 +0200

    refactor: add board size parameter to PlayerBoardImpl

--- src/main/scala/it/unibo/shipps/model/board/PlayerBoard.scala
+++ src/main/scala/it/unibo/shipps/model/board/PlayerBoard.scala
@@ -58,25 +58,25 @@ object PlayerBoard:
     * @param hits the [[Set]] of [[Position]] to initialize the board with.
     * @return a new [[PlayerBoard]] instance.
     */
-  def apply(ships: Set[Ship] = Set.empty, hits: Set[Position] = Set.empty): PlayerBoard =
-    PlayerBoardImpl(ships, hits)
+  def apply(ships: Set[Ship] = Set.empty, hits: Set[Position] = Set.empty, size: Int = 10): PlayerBoard =
+    PlayerBoardImpl(ships, hits, size)
 
-  private case class PlayerBoardImpl(ships: Set[Ship], hits: Set[Position]) extends PlayerBoard:
+  private case class PlayerBoardImpl(ships: Set[Ship], hits: Set[Position], size: Int) extends PlayerBoard:
 
     override def addShip(ship: Ship): Either[String, PlayerBoard] =
       if isAnyPositionOccupied(ship.positions) then
         Left(s"One or more of the following positions are already occupied: ${ship.positions.mkString(", ")}.")
       else
-        Right(PlayerBoardImpl(ships + ship, hits))
+        Right(PlayerBoardImpl(ships + ship, hits, size))
 
     override def removeShip(ship: Ship): Either[String, PlayerBoard] =
       if ships.contains(ship) then
-        Right(PlayerBoardImpl(ships - ship, hits))
+        Right(PlayerBoardImpl(ships - ship, hits, size))
       else
         Left("The ship does not exist on the player board.")
 
     override def hit(target: Position): PlayerBoard =
-      PlayerBoardImpl(ships, hits + target)
+      PlayerBoardImpl(ships, hits + target, size)
 
     override def isAnyPositionOccupied(positions: Set[Position]): Boolean =
       val allShipPositions = ships.flatMap(_.positions)
