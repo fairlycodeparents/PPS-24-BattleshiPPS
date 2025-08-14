7b3ad7f76265f3d891961ce175cef613c11c1a6d
Mirco Terenzi <116552555+mircoterenzi@users.noreply.github.com>
2025-08-14 10:57:36 +0200

    refactor: simplify tests by using BoardCoordinates and ShipPlacementDSL

--- src/test/scala/it/unibo/shipps/logic/UniformDistributionStrategyTest.scala
+++ src/test/scala/it/unibo/shipps/logic/UniformDistributionStrategyTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.*
 import flatspec.*
 import it.unibo.shipps.model.{MaxMinPositionWeighting, UniformDistributionStrategy}
 import it.unibo.shipps.model.board.{PlayerBoard, Position}
+import it.unibo.shipps.model.board.BoardCoordinates.*
 import matchers.*
 
 import scala.language.postfixOps
@@ -14,29 +15,27 @@ class UniformDistributionStrategyTest extends AnyFlatSpec with should.Matchers:
   val strategy: UniformDistributionStrategy = new UniformDistributionStrategy(MaxMinPositionWeighting())
 
   "The strategy" should "return an error when a position is provided for a bot attack" in:
-    val positionToAttack             = Some(Position(5, 5))
     val boardWithNoHits: PlayerBoard = PlayerBoard()
-    val (returnedBoard, result)      = strategy.execute(boardWithNoHits, positionToAttack)
+    val (returnedBoard, result)      = strategy.execute(boardWithNoHits, Some(F(6)))
 
     returnedBoard shouldBe boardWithNoHits
     result.isLeft shouldBe true
 
   it should "return an error when there are no positions left to attack" in:
-    val allPositions: Set[Position] =
-      (for
+    val allPositions =
+      for
         x <- 0 until PlayerBoard.size
         y <- 0 until PlayerBoard.size
-      yield Position(x, y)).toSet
-    val boardWithAllPositionsHit: PlayerBoard = PlayerBoard(hits = allPositions)
+      yield Position(x, y)
+    val boardWithAllPositionsHit: PlayerBoard = PlayerBoard(hits = allPositions.toSet)
     val (returnedBoard, result)               = strategy.execute(boardWithAllPositionsHit, None)
 
     returnedBoard shouldBe boardWithAllPositionsHit
     result.isLeft shouldBe true
 
   it should "choose the farthest corner when only the opposite corner is hit" in:
-    val hitPosition             = Position(0, 0)
-    val boardWithOneHit         = PlayerBoard(hits = Set(hitPosition))
-    val expectedPosition        = Position(PlayerBoard.size - 1, PlayerBoard.size - 1)
+    val boardWithOneHit         = PlayerBoard(hits = Set(A(1)))
+    val expectedPosition        = J(10)
     val (returnedBoard, result) = strategy.execute(boardWithOneHit, None)
     val newHit                  = returnedBoard.hits.diff(boardWithOneHit.hits).head
 
@@ -44,7 +43,7 @@ class UniformDistributionStrategyTest extends AnyFlatSpec with should.Matchers:
     newHit shouldBe expectedPosition
 
   it should "choose a position on the anti-diagonal or center when both main diagonal corners are hit" in:
-    val cornerPositions       = Set(Position(0, 0), Position(PlayerBoard.size - 1, PlayerBoard.size - 1))
+    val cornerPositions       = Set(A(1), J(10))
     val boardWithTwoHits      = PlayerBoard(hits = cornerPositions)
     val expectedBestPositions = for i <- 0 until PlayerBoard.size yield Position(PlayerBoard.size - 1 - i, i)
     val (newBoard, result)    = strategy.execute(boardWithTwoHits, None)
